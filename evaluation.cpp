b1747cc34cbf62d9807b847bda8ca051150a0c62
yattinda <yattinda@gmail.com>
2019-09-03 21:33:30 +0900

    too_complicated

---
+++ evaluation.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int i;
+  cout << "i=20の式は" << (i = 20) << "です" << endl;
+
+  return 0;
+
+}
---
+++ test.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int a,b,c;
+
+  a = 10;
+  b = a + 5;
+  c = b + 15;
+
+
+
+  cout << a << b << c << "happy" << endl;
+
+  return 0;
+
+}
