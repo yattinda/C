9b46495b86f4bdae4d0d0bf45c5e60004a4d6dbe
yattinda <yattinda@gmail.com>
2021-02-11 01:15:27 +0900

    Modify include

--- cpp/timer_con_des.cpp
+++ cpp/timer_con_des.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
@@ -8,20 +9,23 @@ class timer {
   clock_t start;
 
 public:
-  timer()//constructor
-  ~timer()//destructor
+  timer();//constructor
+  ~timer();//destructor
 };
 
 timer::timer(){
-  cout << "計測開始 (コンストラクタ呼び出し)" << endl;
   start = clock();
+  cout << "計測開始 (コンストラクタ呼び出し)" << endl;
+  cout << "start is " << start << endl;
 }
 
 timer::~timer(){
   clock_t end;
   end = clock();
   cout << "計測終了 (デストラクタ呼び出し)" << endl;
-  cout << "経過時間 " << (end - start) / ClOCK_PER_SEC << endl;
+  cout << "end is " << end << endl;
+  cout << "CLOCKS_PER_SEC is " << CLOCKS_PER_SEC << endl;
+  cout << "経過時間 " << (end - start) / CLOCKS_PER_SEC << endl;
 }
 
 int main(){
@@ -29,6 +33,6 @@ int main(){
   char enter;
 
   cout << "Press Enter to stop" << endl;
-  cin >> c;
+  cin >> enter;
   return 0;
 }
