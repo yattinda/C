977132cee7b1019907afe01a91ab76734368ec3b
yattinda <yattinda@gmail.com>
2021-02-10 23:56:41 +0900

    Practice overload

---
+++ cpp/overload_func.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void date(char* date){
+  cout << "日付 " << date << endl;
+}
+
+void date(int year, int month, int day){
+  cout << "日付 " << year << "/" << month << "/" << day << endl;
+}
+
+int main(){
+  date(2020, 02, 10);
+  date("2020/2/10");
+}
